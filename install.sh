e15f2c5d146e763065a1113cfc498895ccc4abdd
Ahn Kiwook <ahnkiwook@gmail.com>
2021-01-22 16:42:51 +0900

    Add vim coc settings

--- install.sh
+++ install.sh
@@ -2,3 +2,13 @@
 cp gitconfig ~/.gitconfig
 cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
+vim \
+    +'PlugInstall' \
+    +'CocInstall coc-tsserver' \
+    +'CocInstall coc-diagnostic' \
+    +'CocInstall coc-eslint' \
+    +'CocInstall coc-jedi' \
+    +'CocInstall coc-json' \
+    +'qa'
+mkdir -p ~/.vim
+cp coc-settings.json ~/.vim/
